6022b4b65fd86da55deab121fbcd782e557facb2
DionStevens <dstevens@roc-dev.com>
2020-09-21 15:06:52 +0200

    Created Op2

---
+++ src/h02/op2.java
@@ -0,0 +1,26 @@
+package h02;
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class op2 extends Applet {
+
+    public void init() {
+        setBackground(Color.white);
+
+
+    }
+
+
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Dion", 50, 50 );
+
+        g.setColor(Color.red);
+        g.drawString("Stevens", 50, 70);
+
+    }
+
+
+}
