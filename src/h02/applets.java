49437c538f65ec5121ee996d1aa6d3c4337e6d6c
DionStevens <dstevens@roc-dev.com>
2020-09-21 14:21:32 +0200

    de Applet works

---
+++ src/h02/applets.java
@@ -0,0 +1,19 @@
+package h02;
+
+import java.awt.*;
+import java.applet.Applet;
+
+public class applets extends Applet {
+
+
+    public void init() {
+    }
+
+
+
+    public void paint(Graphics g) {g.drawString("welcome to java!!", 50, 60);
+
+    }
+
+}
+
