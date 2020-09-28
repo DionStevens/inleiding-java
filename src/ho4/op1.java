024b19d7740e7b668c4bf05bc373238525785b02
DionStevens <dstevens@roc-dev.com>
2020-09-28 12:07:31 +0200

    Created H04 Op 1 test

---
+++ src/ho4/op1.java
@@ -0,0 +1,17 @@
+package ho4;
+
+import java.awt.*;
+import java.applet.Applet;
+
+public class op1 extends Applet {
+
+public void init()  {
+
+}
+
+public void paint(Graphics g)  {
+    g.drawLine(50, 120, 270, 120);
+
+}
+
+}
