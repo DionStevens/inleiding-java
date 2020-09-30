b63486c7cde2174af1c44b4890537b64d7d08623
DionStevens <dstevens@roc-dev.com>
2020-09-30 10:50:10 +0200

    Created and finish op2

---
+++ src/ho4/op3.java
@@ -0,0 +1,27 @@
+package ho4;
+import java.awt.*;
+import java.applet.Applet;
+
+public class op3 extends Applet {
+
+
+    public void init(){
+
+
+    }
+
+    public void paint(Graphics g)    {
+        g.setColor(Color.black);
+        g.fillRect(500, 400, 10, 400);
+        g.setColor(Color.red);
+g.fillRect(300, 400, 200, 20);
+g.setColor(Color.white);
+g.fillRect(300, 420,200,20);
+g.setColor(Color.blue);
+g.fillRect(300,440,200,20);
+
+
+    }
+
+
+}
