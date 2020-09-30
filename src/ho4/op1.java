9985bfaf8e7d30cdbba033c23f5b3887bc2da213
DionStevens <dstevens@roc-dev.com>
2020-09-30 09:45:37 +0200

    Created and finish op2

--- src/ho4/op1.java
+++ src/ho4/op1.java
@@ -11,6 +11,10 @@ public void init()  {
 
 public void paint(Graphics g)  {
     g.drawLine(50, 120, 270, 120);
+g.drawLine(50, 120, 160, 270);
+g.drawLine(270,120, 160, 270);
+
+
 
 }
 
---
+++ src/ho4/op2.java
@@ -0,0 +1,24 @@
+package ho4;
+import java.awt.*;
+import java.applet.Applet;
+
+public class op2 extends Applet {
+
+    public void init( ) {
+
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawRect(250,250,200,200);
+g.drawLine(250,250,350,100);
+g.drawLine(450,250,350,100);
+g.drawRect(350, 370,50, 80);
+g.drawOval(310,280,50,50);
+
+
+
+    }
+
+
+}
