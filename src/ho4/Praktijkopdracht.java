2694ddda3f545ef6a0bf0ca8a4c7e718059ffa38
DionStevens <dstevens@roc-dev.com>
2020-10-01 11:22:48 +0200

    Praktijkopdracht afgemaakt

--- src/ho4/Praktijkopdracht.java
+++ src/ho4/Praktijkopdracht.java
@@ -6,6 +6,7 @@ import java.applet.Applet;
 public class Praktijkopdracht extends Applet {
 
     public void init (){
+        setSize(1600,600);
 
 
 
@@ -16,7 +17,10 @@ public class Praktijkopdracht extends Applet {
         g.drawLine(60,60, 310,60);
         g.drawRect(60,100,250,150);
         g.drawRoundRect(60,290,250,150,30,30);
-        //help
+        g.setColor(Color.magenta);
+        g.fillRect(330,100,250,150);
+        g.setColor(Color.black);
+        g.drawOval(330,100,250,150);
         g.setColor(Color.magenta);
         g.fillOval(330,290,250,150);
         g.setColor(Color.black);
@@ -25,6 +29,14 @@ public class Praktijkopdracht extends Applet {
         g.fillArc(600,100,250,150,0,45);
         g.setColor(Color.black);
         g.drawOval(650,290,150,150);
+        g.drawString("lijn",160,80);
+        g.drawString("Rechthoek",160,270);
+        g.drawString("Afgeronde rechthoek", 130, 460);
+        g.drawString("Gevulde rechthoek met ovaal",360,270);
+        g.drawString("Gevulde ovaal",400,460);
+        g.drawString("Taartpunt met ovaal eromheen",640,270);
+        g.drawString("cirkel", 720,460);
+
 
 
 
