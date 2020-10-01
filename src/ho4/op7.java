7b2363a3cfeb64815f9ababf58d5826a7220164b
DionStevens <dstevens@roc-dev.com>
2020-10-01 09:55:15 +0200

    Created op7

---
+++ src/ho4/op7.java
@@ -0,0 +1,28 @@
+package ho4;
+
+import java.awt.*;
+import java.applet.Applet;
+
+public class op7 extends Applet {
+
+    public void init(){
+
+
+    }
+
+
+    public void paint(Graphics g) {
+        g.drawRect(250,250,300,300);
+        g.fillOval(300,300,75,75);
+        g.fillOval(425,300,75,75);
+        g.fillOval(300,425,75,75);
+        g.fillOval(425,425,75,75);
+
+
+
+
+    }
+
+
+
+}
