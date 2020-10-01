cd1ebccd0c3e4c678de89d2239117015e44c51cc
DionStevens <dstevens@roc-dev.com>
2020-10-01 08:30:17 +0200

    Created and op5

---
+++ src/ho4/op5.java
@@ -0,0 +1,24 @@
+package ho4;
+
+import java.awt.*;
+import java.applet.Applet;
+
+public class op5 extends Applet {
+
+
+    public void init(){
+
+
+
+    }
+
+
+    public void paint (Graphics g) {
+
+        setBackground(Color.blue);
+        g.setColor(Color.yellow);
+g.fillArc(250,250,250,250,0,360);
+
+    }
+
+}
