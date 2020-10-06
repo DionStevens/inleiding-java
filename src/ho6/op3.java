ed86f2433bfcef51218badf11fd3e71dd6628c32
DionStevens <dstevens@roc-dev.com>
2020-10-06 16:05:57 +0200

    made h6 ik heb geen idee hoe ik op3 moet maken maar ik heb tot nu toe dit

---
+++ src/ho6/op3.java
@@ -0,0 +1,27 @@
+package ho6;
+
+import java.awt.*;
+import java.applet.Applet;
+
+public class op3 extends Applet {
+    int Positiev1;
+    int Positiev2;
+    int Negatief;
+
+    public void init(){
+        Positiev1 =
+        Positiev2 =
+        Negatief =
+
+    }
+public void paint (Graphics g){
+        g.drawString("het eerste positieven getal is"  , 50,50);
+        g.drawString("het tweede positieven getal is",50,70);
+        g.drawString("het negatieven getal dat ze samen maken is",50,90);
+
+
+}
+
+
+
+}
