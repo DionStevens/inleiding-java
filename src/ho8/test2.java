b63854185b0a5d4fdd7893efa44a6ff2a4eda830
DionStevens <dstevens@roc-dev.com>
2020-10-07 13:29:44 +0200

    test2 deel3 gemaakt

--- src/ho8/test2.java
+++ src/ho8/test2.java
@@ -2,25 +2,44 @@ package ho8;
 
 import java.awt.*;
 import java.applet.Applet;
+import java.awt.event.*;
 
 public class test2 extends Applet {
     TextField tekstvak;
     Label label;
+    String s;
 
 
 
     public void init(){
         tekstvak = new TextField("klik op mij", 20);
-        label = new Label("Type iets in het tekstvakje");
+        label = new Label("Type iets in het tekstvak" +
+                "en druk op enter");
+        tekstvak.addActionListener( new TekstvakListener ( ) );
         add(label);
         add(tekstvak);
+        s = "";
 
     }
 
     public void paint(Graphics g){
+        g.drawString(s,50,60);
+
+    }
+
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            s = tekstvak.getText();
+            repaint();
+
+
+        }
+
+
 
 
     }
+
 
 
 }
