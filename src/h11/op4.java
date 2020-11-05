eeb72e4e6993e52e53abcc1afbb50c7a5b5e4dba
DionStevens <dstevens@roc-dev.com>
2020-11-05 09:44:33 +0100

    H11 op4 gemaakt

--- src/h11/op4.java
+++ src/h11/op4.java
@@ -1,4 +1,38 @@
 package h11;
 
-public class op4 {
+import java.awt.*;
+import java.applet.Applet;
+
+public class op4 extends Applet {
+
+    public void init(){}
+
+    public void paint(Graphics g){
+        int teller;
+        int y = 0;
+        int eersteKeerGetal = 3;
+        int tweedeKeerGetal = 1;
+        int huidigeGetal;
+
+        for(teller = 0; teller < 10; teller ++){
+            y += 20;
+            huidigeGetal = eersteKeerGetal * tweedeKeerGetal;
+            tweedeKeerGetal++;
+            g.drawString("" + huidigeGetal, 305,y);
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+
 }
