c2c9ab4180778f1a80fe26b8035b35c80d3c0770
DionStevens <dstevens@roc-dev.com>
2020-11-05 08:36:27 +0100

    H11 op1 gemaakt

--- src/h11/op1.java
+++ src/h11/op1.java
@@ -4,4 +4,27 @@ import java.awt.*;
 import java.applet.Applet;
 
 public class op1 extends Applet {
+
+
+    public void init (){}
+
+
+
+    public void paint (Graphics g) {
+        int teller;
+        int x = 0;
+
+
+        for (teller = 0; teller < 10; teller++) {
+            x += 20;
+            g.drawLine(x, 50,x,300);
+            g.drawString("" + teller, x,315);
+
+        }
+
+    }
+
+
+
+
 }
