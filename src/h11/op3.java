ca5b81171c29565d019033112660dc3554fcfe15
DionStevens <dstevens@roc-dev.com>
2020-11-05 09:37:59 +0100

    H11 op3 afgemaakt

--- src/h11/op3.java
+++ src/h11/op3.java
@@ -10,12 +10,19 @@ public class op3 extends Applet {
 
     public void paint(Graphics g){
         int teller;
-        int y = 0;
+        int y = 40;
+        int tweeNaVorigeGetal = 1;
+        int vorigeGetal =1;
+        int huidigeGetal;
+        g.drawString("1", 305,20);
+        g.drawString("1",305,40);
 
-
-        for(teller = 1; teller< 100; teller++){
+        for(teller = 1; teller< 20; teller++){
+            huidigeGetal = tweeNaVorigeGetal + vorigeGetal;
             y += 20;
-            g.drawString("" + teller, 305,y);
+            g.drawString("" + huidigeGetal, 305,y);
+            tweeNaVorigeGetal = vorigeGetal;
+            vorigeGetal = huidigeGetal;
 
 
 
