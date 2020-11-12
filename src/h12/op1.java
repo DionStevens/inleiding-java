424ea01e7fe13fb531954fd4d9a7dfff443aa02d
DionStevens <dstevens@roc-dev.com>
2020-11-12 09:19:09 +0100

    H12 op1 afgemaakt

--- src/h12/op1.java
+++ src/h12/op1.java
@@ -4,26 +4,37 @@ import java.awt.*;
 import java.applet.*;
 
 public class op1 extends Applet {
-    double[] getallen;
-    //double Gemiddeld[];
+    int[] huidigGetal;
+    double Gemiddeld;
+    int vorigGetal;
 
     public void init() {
-        getallen = new double [10];
+        huidigGetal = new int [10];
 
 
 
-        for (int teller = 0; teller < getallen.length; teller ++) {
-            getallen[teller] = 10 * teller + 10;
+
+        for (int teller = 0; teller < huidigGetal.length; teller ++) {
+            huidigGetal[teller] = 10 * teller + 10;
 
         }
 
     }
 
     public void paint (Graphics g) {
-        for (int teller = 0; teller < getallen.length; teller ++) {
-            g.drawString("" + getallen[teller], 50, 20 * teller + 20);
+
+
+
+
+        for (int teller = 0; teller < huidigGetal.length; teller ++) {
+
+            g.drawString("" + huidigGetal[teller], 50, 20 * teller + 20);
+            vorigGetal += huidigGetal[teller];
+
 
         }
+        Gemiddeld = vorigGetal * 0.10;
+        g.drawString("het gemiddelde is " + Gemiddeld,80,20);
 
 
     }
