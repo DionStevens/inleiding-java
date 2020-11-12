dc8db52bff92a6bf5591f036731b247eb470e9f3
DionStevens <dstevens@roc-dev.com>
2020-11-12 10:43:31 +0100

    H12 op2 nu is het wel klaar

--- src/h12/op2.java
+++ src/h12/op2.java
@@ -8,30 +8,23 @@ public class op2 extends Applet {
     Button test;
 
 
-
-
     public void init () {
+        setSize(1000,1000);
         knoppen = new Button[25];
 
 
+        for (int teller = 0; teller < knoppen.length; teller ++) {
+            test = new Button("test");
+            add(test);
 
+
+        }
     }
+
 
     public void paint(Graphics g) {
-        for (int teller = 0; teller < knoppen.length; teller ++) {
-
-            test = new Button();
-            test.setLabel("test");
-            add(test);
-            repaint();
-        }
-
 
 
     }
-
-
-
-
 
 }
